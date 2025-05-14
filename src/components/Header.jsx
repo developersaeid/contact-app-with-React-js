dc0c6277c4f3eb1be499167ff8da153057980ab1
Saeid Behjat <saeiddeveloper@yahoo.com>
2025-05-14 11:54:02 +0330

    Update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className={styles.container}>
       <h1>Contact App</h1>
-      <p><a href="https://botostart">Botostart</a> | React.js Full Course</p>
+      <p><a href="https://react.dev/">React.js</a> Project</p>
     </div>
   )
 }
