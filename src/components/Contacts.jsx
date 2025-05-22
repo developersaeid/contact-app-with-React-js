4b45e9dc348f9a600f2da4ba33fd993752ee8a3c
Saeid Behjat <saeiddeveloper@yahoo.com>
2025-05-22 23:32:43 +0330

    Update Contacts.jsx

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -7,19 +7,20 @@ import styles from "./Contacts.module.css"
 
 function Contacts() {
   const [alert, setAlert] = useState(""); // state to store alert message
-  const [contacts, setContacts] = useState([]); // for all our Contact's
+  const [contacts, setContacts] = useState([]); // State to store contacts
   const [contact, setContact] = useState({
     id: "",
     name: "",
     lastName: "",
     email: "",
     phone: "",
-  });
+  }); // State to store contact details
 
+   // With this function, every time the user types a character in an input field, the contact state gets updated dynamically.
   const changeHandler = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setContact((contact) => ({ ...contact, [name]: value })); //update state
+    const name = event.target.name; // The name of the field that was changed.
+    const value = event.target.value; // The new value entered by the user in input.
+    setContact((contact) => ({ ...contact, [name]: value }));  // Then the field that was changed will be updated state with the new value.
   };
   const addHandler = () => {
     // add contact to contacts array
@@ -32,20 +33,20 @@ function Contacts() {
       setAlert("Please fill all fields");
       return;
     }
-    setAlert("");
+    setAlert(""); // clear alert
     const newContact = {...contact,id:v4() } // create new contact with id
-    setContacts((contacts) => [...contacts, newContact]);
+    setContacts((contacts) => [...contacts, newContact]);// add new contact to contacts array 
     setContact({
       id:"",
       name: "",
       lastName: "",
       email: "",
       phone: "",
-    });
+    });  // reset contact state
   };
 
   const deleteHandler = (id) => {
-    const newContacts = contacts.filter(contact => contact.id !== id);
+    const newContacts = contacts.filter(contact => contact.id !== id); // filter contacts array
     setContacts(newContacts);
   }
 
@@ -58,7 +59,7 @@ function Contacts() {
             type={input.type}
             placeholder={input.placeholder}
             name={input.name}
-            value={contact[input.name]}
+            value={contact[input.name]} // read value from state contact
             onChange={changeHandler}
           />
         ))}
