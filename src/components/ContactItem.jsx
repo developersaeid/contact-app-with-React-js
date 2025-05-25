7432045fb2528a8ee3c852b86ddf0e2d00bf2e0e
Saeid Behjat <saeiddeveloper@yahoo.com>
2025-05-25 14:22:40 +0330

    resopnsive

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -4,7 +4,7 @@ function ContactItem({data:{id,name,lastName,email,phone},deleteHandler}) {
   return (
     <li className={Styles.item}>
             <p>{name} {lastName}</p>
-            <p><span>📬</span> {email}</p>
+            <p><span className={Styles.spanItem}>📬</span> {email}</p>
             <p><span>📞</span> {phone}</p>
             <button onClick={()=>deleteHandler(id)} >❌</button>
     </li>
